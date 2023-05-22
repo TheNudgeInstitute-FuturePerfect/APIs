2035d11882e2fbca1ccfba08c0090019cb06b99b
Aashutosh Chaudhary <aashutosh@instasalary.app>
2023-05-22 22:29:55 +0530

    fix

--- telegram-client/index.mjs
+++ telegram-client/index.mjs
@@ -6,67 +6,74 @@ const router = express.Router();
 
 // receive telegram event
 router.post("/event", async (req, res) => {
-  const { name, call, participants } = req.body;
-  // video call
-  if (name === "UpdateGroupCall" && call) {
-    // started
-    if (
-      call.name === "GroupCall" &&
-      call.participants_count == 0 &&
-      call.version == 1
-    ) {
-      await TelegramVideoChat.create({ callId: call.id });
+  const { name, call, participants, chat_id } = req.body;
+  // Jobcoach Dev - 1963078754
+  // Jobcoach UAT - 956265219
+  if ([1963078754, 956265219].includes(chat_id)) {
+    // video call
+    if (name === "UpdateGroupCall" && call) {
+      // started
+      if (
+        call.name === "GroupCall" &&
+        call.participants_count == 0 &&
+        call.version == 1
+      ) {
+        await TelegramVideoChat.create({ callId: call.id });
+      }
     }
-  }
-  // ended
-  if (call && call.name === "GroupCallDiscarded") {
-    const telegramVideoChat = await TelegramVideoChat.findOne({
-      callId: call.id,
-    });
-    if (telegramVideoChat) {
-      telegramVideoChat.endAt = new Date();
-      await telegramVideoChat.save();
-      await TelegramVideoChatParticipant.updateMany(
-        {
-          callId: call.id,
-          leftAt: null,
-        },
-        { leftAt: new Date() }
-      );
+    // ended
+    if (call && call.name === "GroupCallDiscarded") {
+      const telegramVideoChat = await TelegramVideoChat.findOne({
+        callId: call.id,
+      });
+      if (telegramVideoChat) {
+        telegramVideoChat.endAt = new Date();
+        await telegramVideoChat.save();
+        await TelegramVideoChatParticipant.updateMany(
+          {
+            callId: call.id,
+            leftAt: null,
+          },
+          { leftAt: new Date() }
+        );
+      }
     }
   }
 
   // participant
   if (call && name === "UpdateGroupCallParticipants" && participants.length) {
-    for (let i = 0; i < participants.length; i++) {
-      if (participants[i].peer && participants[i].peer.user_id) {
-        if (participants[i].just_joined) {
-          console.log("joined");
-          // joined
-          const telegramVideoChatParticipant =
-            await TelegramVideoChatParticipant.create({
-              callId: call.id,
-              userId: participants[i].peer.user_id,
-            });
-          console.log(telegramVideoChatParticipant);
-        }
-        // left
-        if (participants[i].left) {
-          const telegramVideoChatParticipant =
-            await TelegramVideoChatParticipant.findOne({
-              callId: call.id,
-              userId: participants[i].peer.user_id,
-              leftAt: null,
-            });
-          if (telegramVideoChatParticipant) {
-            telegramVideoChatParticipant.leftAt = new Date();
-            await telegramVideoChatParticipant.save();
+    const telegramVideoChat = await TelegramVideoChat.findOne({
+      callId: call.id,
+    });
+    if (telegramVideoChat) {
+      for (let i = 0; i < participants.length; i++) {
+        if (participants[i].peer && participants[i].peer.user_id) {
+          if (participants[i].just_joined) {
+            // joined
+            const telegramVideoChatParticipant =
+              await TelegramVideoChatParticipant.create({
+                callId: call.id,
+                userId: participants[i].peer.user_id,
+              });
+          }
+          // left
+          if (participants[i].left) {
+            const telegramVideoChatParticipant =
+              await TelegramVideoChatParticipant.findOne({
+                callId: call.id,
+                userId: participants[i].peer.user_id,
+                leftAt: null,
+              });
+            if (telegramVideoChatParticipant) {
+              telegramVideoChatParticipant.leftAt = new Date();
+              await telegramVideoChatParticipant.save();
+            }
           }
         }
       }
     }
   }
-  // console.log(req.body);
+  console.log(req.body);
   res.json({ success: true });
 });
 
